65e219b4c716ebee78b221a9f74d45d2b7f1eb3e
Isoboi <isoboy.k@gmail.com>
2023-02-28 12:58:48 +0500

    1)Создал компонент hotel list и добавил в модуль home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { HotelListComponent } from '../components/hotel-list/hotel-list.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, HotelListComponent],
   imports: [SharedModule, HomeRoutingModule],
 })
 export class HomeModule {}
