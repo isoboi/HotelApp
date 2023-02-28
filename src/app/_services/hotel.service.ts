c97b724d14b9f3fda0852576f40d2de10ef535fd
Isoboi <isoboy.k@gmail.com>
2023-02-28 13:14:41 +0500

    3)Создал сервис с методами для получения, добавления и удаления гостиницы, а также для получения и добавления категорий

---
+++ src/app/_services/hotel.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { HttpDataResp } from '../_models/http-data-resp';
+import { HotelModel } from '../_models/hotel-model';
+import { HotelCategoryModel } from '../_models/hotel-category-model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HotelService {
+  private _url = environment.apis.default.url;
+
+  constructor(
+    private _http: HttpClient
+  ) {
+  }
+
+  getAllHotels(): Observable<HttpDataResp<HotelModel>> {
+
+    return this._http.get<HttpDataResp<HotelModel>>(this._url + '/api/app/hotel');
+  }
+
+  createHotel(body: Partial<HotelModel>): Observable<HotelModel> {
+    return this._http.post<HotelModel>(this._url + 'api/app/hotel', body);
+  }
+
+  deleteHotel(hotelId: string): Observable<any> {
+    return this._http.delete<any>(this._url + `api/app/hotel/${hotelId}`);
+  }
+
+  getHotelCategories(hotelId: string): Observable<HttpDataResp<HotelCategoryModel>> {
+    return this._http.get<HttpDataResp<HotelCategoryModel>>(this._url + '/api/app/category');
+  }
+
+  createHotelCategory(body: Partial<HotelCategoryModel>): Observable<HotelCategoryModel> {
+    return this._http.post<HotelCategoryModel>(this._url + 'api/app/category', body);
+  }
+
+}
--- src/app/components/hotel-list/hotel-list.component.ts
+++ src/app/components/hotel-list/hotel-list.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HotelModel } from '../../_models/hotel-model';
+import { HotelService } from '../../_services/hotel.service';
+import { HttpErrResp } from '../../_models/http-err-resp';
+import { catchError, of } from 'rxjs';
+import { HttpDataResp } from '../../_models/http-data-resp';
 
 @Component({
   selector: 'app-hotel-list',
   templateUrl: './hotel-list.component.html',
   styleUrls: ['./hotel-list.component.scss']
 })
-export class HotelListComponent {
+export class HotelListComponent implements OnInit {
+  public hotelList: Array<HotelModel> = [];
 
+  constructor(
+    private _hotelService: HotelService
+  ) {
+  }
+
+  ngOnInit() {
+    this.getHotelList();
+  }
+
+  getHotelList(): void {
+    this._hotelService.getAllHotels()
+      .pipe(
+        catchError((err: HttpErrResp) => {
+          return of(false);
+        })
+      )
+      .subscribe((res: HttpDataResp<HotelModel>) => {
+        if (res) {
+          this.hotelList = res.items;
+        }
+      });
+  }
 }
